cf83761f694cf6a083955dce5d81def66dab1558
MachineCreation <142163578+MachineCreation@users.noreply.github.com>
2024-08-23 09:09:23 -0700

    oops...rollback

--- TRAJ_Hub-BE/app/main/routes.py
+++ TRAJ_Hub-BE/app/main/routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, jsonify, request
+from flask import Blueprint, render_template, jsonify, request, current_app
 from flask_login import login_required, current_user
-from models import supabase, User
+from models import supabase, supabase_service, User
 import os
 
 
@@ -12,11 +12,3 @@ def main_page():
     user_data = User.get(current_user.id)[1]
     return render_template('main.html', user_data=user_data)
 
-@main_bp.route('/')
-@login_required
-def home():
-    return render_template('upload.html')
-
-@main_bp.route('/hero-form', methods=['POST'])
-def hero_submit():
-    
