4b01ab711ce70aef4f14bd724fc8246bbbc9c71e
MachineCreation <142163578+MachineCreation@users.noreply.github.com>
2024-09-06 17:09:38 -0700

    perk image updates

--- TRAJ_Hub-FE/src/components/quad-squad.tsx
+++ TRAJ_Hub-FE/src/components/quad-squad.tsx
@@ -1,4 +1,5 @@
 // React
+import { useNavigate } from 'react-router-dom';
 
 // components
 
@@ -7,13 +8,19 @@ import "../css/QuadSquad.css"
 
 const QuadSquad = () => {
 
+    const navigate = useNavigate();
+
+    const navClick = (nav: string) => (_event: React.MouseEvent<HTMLDivElement>) => {
+        navigate(nav);
+    }
+
     return(
         <div className="absolute flex justify-items-center w-full h-screen">
             <section className="flex w-full max-w-5xl h-screen m-auto overflow-hidden relative">
-                <figure id="avi-t"></figure>
-                <figure id="avi-r"></figure>
-                <figure id="avi-a"></figure>
-                <figure id="avi-j"></figure>
+                <figure id="avi-t" onClick={navClick("/topsloth")} className='cursor-pointer z-10'></figure>
+                <figure id="avi-r" onClick={navClick("/RedPrimeOrigin")} className='cursor-pointer z-10'></figure>
+                <figure id="avi-a" onClick={navClick("/araspberryberet")} className='cursor-pointer z-10'></figure>
+                <figure id="avi-j" onClick={navClick("/JesusTts")} className='cursor-pointer z-10'></figure>
             </section>
         </div>
     );
